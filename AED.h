1243295aac6a36497c9577d004034d22980292ea
alumno <alumno@Mac-mini-de-administrador-5.local>
2015-02-02 11:55:13 +0100

    "a<sdasd"

---
+++ AED.h
@@ -0,0 +1,13 @@
+//
+//  AED.h
+//  SuperKoalio
+//
+//  Created by alumno on 2/2/15.
+//  Copyright (c) 2015 Razeware. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AED : NSObject
+
+@end
